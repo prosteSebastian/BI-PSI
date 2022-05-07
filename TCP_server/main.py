a6735e5f0ee43c7ba855fccfa1cc69cfe8a9b5c7
Sebastian Prokop <prokoseb@fit.cvut.cz>
2022-05-07 15:52:39 +0200

    revieving messagges

--- TCP_server/main.py
+++ TCP_server/main.py
@@ -7,9 +7,12 @@ import threading
 #def robot_more():
 
 
+
+
+
 IP = "127.0.0.1" #global variables
 PORT = 3999
-BUFFER = 1
+BUFFER = 255
 END = "\a\b"
 
 #server messages
@@ -29,19 +32,57 @@ SERVER_KEY_OUT_OF_RANGE_ERROR = "303 KEY OUT OF RANGE"+ END
 TIMEOUT = 1
 TIMEOUT_RECHARGING = 5
 
-def autentication():
+aut_array_s = [23019,3203, 18789, 16443, 18189]
+aut_array_c = [32037, 29295, 13603, 29533, 21952]
+
+def hash(string):
+
+    sum = 0
+    for i in range(0, len(string) - 1):
+        sum=+ord(string[i])
+
+    sum = (sum*1000)%2**16
+    return sum
 
 
-if __name__ == '__main__':
-    s  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((IP,PORT))
+def authentication():
+    pass
+
+str = ""
+
+def extract_message():
+    global str
+    lmao_END = str.find(END)
+    return_val = str[0:lmao_END]
+
+    str = str[END+2:]
+    return return_val
+
+def correct_message(conn):
+    global str
+    while str.find(END) ==-1:
+
+        data = conn.recv(BUFFER)
+        str = str + (data.decode('ascii'))
+        print("recieved:", data)
+
+
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
+    s.bind((IP, PORT))
+
     s.listen(1)
     print("listening")
     conn, addr = s.accept()
-    while 420:
-        data  = conn.recv(BUFFER)
-        if not data:
-            break
-        print("recieved:",data)
-        conn.send(data)
+    correct_message(conn)
+    # while 420:
+    #
+    #
+    #     conn.send(data)
     conn.close()
+
+
+if __name__ == '__main__':
+    main()
